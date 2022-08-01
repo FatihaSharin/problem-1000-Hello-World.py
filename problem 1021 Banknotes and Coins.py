56bb1cf149a9d141d3e66fbcd30f8eb68899961b
FatihaSharin <109498702+FatihaSharin@users.noreply.github.com>
2022-08-01 17:32:45 +0600

    Create problem 1021 Banknotes and Coins.py

---
+++ problem 1021 Banknotes and Coins.py
@@ -0,0 +1,57 @@
+a= float(input())
+t=a
+
+b=t//100
+f_h=t%100
+
+c=f_h//50
+d=f_h%50
+
+e=d//20
+f=d%20
+
+g=f//10
+h=f%10
+
+i=h//5
+j=h%5
+
+k=j//2
+l=j%2
+
+
+
+
+print("NOTAS:")
+print(int(b),"nota(s) de R$ 100.00")
+print(int(c),"nota(s) de R$ 50.00")
+print(int(e),"nota(s) de R$ 20.00")
+print(int(g),"nota(s) de R$ 10.00")
+print(int(i),"nota(s) de R$ 5.00")
+print(int(k),"nota(s) de R$ 2.00")
+
+
+
+m=a*100
+n=int(m)
+o=n%100
+p=o/50
+q=o%50
+r=q/25
+s=q%25
+t=s/10
+u=s%10
+v=u/5
+w=u%5
+x=w/1
+y=w%1
+
+
+
+print("MOEDAS:")
+print(int(l),"moeda(s) de R$ 1.00")
+print(int(p),"moeda(s) de R$ 0.50")
+print(int(r),"moeda(s) de R$ 0.25")
+print(int(t),"moeda(s) de R$ 0.10")
+print(int(v),"moeda(s) de R$ 0.05")
+print(int(x),"moeda(s) de R$ 0.01")
