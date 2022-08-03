caeae2877867be830a99df84b55f636efc06a9a6
FatihaSharin <109498702+FatihaSharin@users.noreply.github.com>
2022-08-03 18:32:16 +0600

    Create problem 1038 Bhaskara's Formula.py

---
+++ problem 1038 Bhaskara's Formula.py
@@ -0,0 +1,14 @@
+import math
+
+A, B, C = map(float,input().split())
+
+D = (B ** 2) - (4 * A * C)
+
+if(D < 0 or A == 0):
+    print("Impossivel calcular")
+else:
+    D = math.sqrt(D)
+    R1 = (- B + D) / (2 * A)
+    R2 = (- B - D) / (2 * A)
+    print("R1 = %0.5f"%R1)
+    print("R2 = %0.5f"%R2)
