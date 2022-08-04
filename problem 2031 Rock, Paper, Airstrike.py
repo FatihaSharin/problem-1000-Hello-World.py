42ad2e959eb71c7eac6e0c5cf5bc786638576cf3
FatihaSharin <109498702+FatihaSharin@users.noreply.github.com>
2022-08-04 19:14:09 +0600

    Create problem 2031 Rock, Paper, Airstrike.py

---
+++ problem 2031 Rock, Paper, Airstrike.py
@@ -0,0 +1,22 @@
+n = int(input())
+while(n > 0):
+    n -= 1
+    p1 = input()
+    p2 = input()
+    if(p1 == 'ataque'):
+        if(p2 == 'ataque'):
+            print('Aniquilacao mutua')
+        else:
+            print('Jogador 1 venceu')
+    elif(p1 == 'papel'):
+        if((p2 == 'ataque') or (p2 == 'pedra')):
+            print('Jogador 2 venceu')
+        else:
+            print('Ambos venceram')
+    elif(p1 == 'pedra'):
+        if(p2 == 'papel'):
+            print('Jogador 1 venceu')
+        if(p2 == 'ataque'):
+            print('Jogador 2 venceu')
+        elif(p2 == 'pedra'):
+            print('Sem ganhador')
