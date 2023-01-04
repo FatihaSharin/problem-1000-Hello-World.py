8c2e5baeaf8ad8c2f98eb881629319baa0a37bdf
FatihaSharin <109498702+FatihaSharin@users.noreply.github.com>
2023-01-04 17:00:15 +0600

    Update problem 1008 Salary.py

--- problem 1008 Salary.py
+++ problem 1008 Salary.py
@@ -1,8 +1,8 @@
-num= int(input())
-hour= int(input())
-ammountph= float(input())
+num = int(input())
+hour = int(input())
+ph = float(input())
 
-S=float(ammountph*hour)
+S = float(ph*hour)
 
 print("NUMBER = {}".format(num))
 print("SALARY = U$ %0.2f"%S)
