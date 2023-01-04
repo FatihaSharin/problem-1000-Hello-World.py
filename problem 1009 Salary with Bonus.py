0ef7d7dc31eda6abd31c4b6315000f4284e90179
FatihaSharin <109498702+FatihaSharin@users.noreply.github.com>
2023-01-04 17:03:54 +0600

    Update problem 1009 Salary with Bonus.py

--- problem 1009 Salary with Bonus.py
+++ problem 1009 Salary with Bonus.py
@@ -1,8 +1,8 @@
-name= input()
-salary= float(input())
-sold_value= float(input())
+name = input()
+salary = float(input())
+bonus = float(input())
 
-a= sold_value*0.15
-total= salary+a
+a= bonus * 0.15
+total = salary + a
 
 print("TOTAL = R$ %0.2f"%total)
